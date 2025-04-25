622dc8ee2e00177cc6d2378a88baaa1d3a24e14f
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-04-25 16:43:57 +0200

    Terminado Ejercicio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
-
+import { Header } from './componentes/header'
+import { Usuarios } from './componentes/Usuarios'
+import { Routes, Route } from 'react-router-dom'
+import { Camareros } from './componentes/Camareros'
 function App() {
-
   return (
     <>
-    <header>David Henrique</header>
-    <main> Yo soy locura</main>
-    <footer>Soy footer</footer>
-      
+      <Header />
+      <main className="container my-4 pt-3">
+        <Routes>
+          <Route path="/camareros" element={<Camareros />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+        </Routes>
+      </main>
+
+      <footer >
+      </footer>
     </>
   )
 }
